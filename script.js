24ad020e0f26d1a29af7dff5f68342d0bbd73e31
Praise Ziome <ziomepraiser@gmail.com>
2025-06-20 15:23:28 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+async function getWeather() {
+  const city = document.getElementById("city").value;
+  const apiKey = "e3d4724a0916f137af03830dd340ded3";
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${Kariba}&appid=${apiKey}&units=metric`;
+
+  try {
+    const response = await fetch(url);
+    if (!response.ok) throw new Error("City not found");
+    const data = await response.json();
+
+    document.getElementById("result").innerHTML = `
+      <strong>${data.name}</strong><br>
+      ${data.weather[0].main} - ${data.main.temp}°C
+    `;
+  } catch (error) {
+    document.getElementById("result").innerHTML = "Error: " + error.message;
+  }
+}
